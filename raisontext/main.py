c9af9a968c3a28502ed093cb20ca327521c4e95c
Tay Jen <helloevgenyy@gmail.com>
2023-12-03 14:37:14 +0300

    async is the head

--- raisontext/main.py
+++ raisontext/main.py
@@ -29,15 +29,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-# url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
-# print('Here I am')
-url = "amqps://efiosdwv:5Mg1vBx3EbTu9RB4mRpj7xEOZh5XKXoz@cow.rmq2.cloudamqp.com/efiosdwv"
-params = pika.URLParameters(url)
-connection = pika.BlockingConnection(params)
-channel_send_request = connection.channel()  # start a channel
-channel_send_request.queue_declare(queue="raisontext_model", durable=True)  # Declare a queue
-
 
 def print_function(message: dict):
     print('Here we got incoming message %s', message)
@@ -45,8 +36,8 @@ def print_function(message: dict):
 
 pika_client = PikaClient(
     print_function,
-    "raisontext_model",
-    "raisontext_answer"
+    publish_queue_name="raisontext_model",
+    consume_queue_name="raisontext_answer"
 )
 
 
@@ -54,24 +45,10 @@ class ClassifyText(BaseModel):
     text: str = ""
 
 
-async def start():
-    print("Here")
-    channel_get_answer = connection.channel()  # start a channel
-    channel_get_answer.queue_declare(queue="raisontext_answer", durable=True)  # Declare a queue
-
-    def callback(ch, method, properties, body):
-        print("In callback")
-        print("[x] Received " + str(body))
-
-    channel_get_answer.basic_consume('raisontext_answer', callback, auto_ack=True)
-    await channel_get_answer.start_consuming()
-    print("Not here")
-
-
 @app.on_event("startup")
 async def startup():
     loop = asyncio.get_running_loop()
-    task = loop.create_task(foo_app.pika_client.consume(loop))
+    task = loop.create_task(pika_client.consume(loop))
     await task
 
 
@@ -89,25 +66,13 @@ async def classify(payload: ClassifyText = None) -> Dict[str, str]:
     print(text)
 
     if text:
-        # evaluator = RaisonText()
-
-        # MODELS_DIR = os.path.join(config.REPO_ROOT, 'models')
-        # model_dir = MODELS_DIR / 'TfIdfLogReg_first silly baseline'
-        # model_name = '4a015324-601a-43d3-9df9-856323b97f0d.pkl'
-        # with open(model_dir / model_name, 'rb') as f:
-        #     model = pickle.load(f)
-
-        channel_send_request.basic_publish(
-            exchange='',
-            routing_key='raisontext_model',
-            body=text
+        pika_client.send_message(
+            {
+                "text": text
+            }
         )
 
         print(f"[x] Sent {text}")
-        # connection.close()
-
-        # prediction = evaluator.evaluate_quality(text, Classifier.NaiveClassifier)
-
         prediction = 0.5
         status = 'OK'
     else:
@@ -120,5 +85,3 @@ async def classify(payload: ClassifyText = None) -> Dict[str, str]:
         'status': status,
         'prediction': str(prediction)
     }
-
-start()
--- raisontext/pika_client.py
+++ raisontext/pika_client.py
@@ -13,8 +13,8 @@ class PikaClient:
             pika.URLParameters("amqps://efiosdwv:5Mg1vBx3EbTu9RB4mRpj7xEOZh5XKXoz@cow.rmq2.cloudamqp.com/efiosdwv")
         )
         self.channel = self.connection.channel()
-        self.publish_queue = self.channel.queue_declare(queue=publish_queue_name)
-        self.consume_queue = self.channel.queue_declare(queue=consume_queue_name)
+        self.publish_queue = self.channel.queue_declare(queue=publish_queue_name, durable=True)
+        self.consume_queue = self.channel.queue_declare(queue=consume_queue_name, durable=True)
 
         self.publish_callback_queue = self.publish_queue.method.queue
         self.consume_callback_queue = self.consume_queue.method.queue
@@ -24,15 +24,20 @@ class PikaClient:
 
     async def consume(self, loop):
         """Setup message listener with the current running loop"""
-        connection = await connect_robust(host="amqps://efiosdwv:5Mg1vBx3EbTu9RB4mRpj7xEOZh5XKXoz@cow.rmq2.cloudamqp.com/efiosdwv", loop=loop)
+        connection = await connect_robust(
+            # host="amqps://efiosdwv:5Mg1vBx3EbTu9RB4mRpj7xEOZh5XKXoz@cow.rmq2.cloudamqp.com/efiosdwv",
+            "amqps://efiosdwv:5Mg1vBx3EbTu9RB4mRpj7xEOZh5XKXoz@cow.rmq2.cloudamqp.com/efiosdwv",
+            loop=loop
+        )
+
         channel = await connection.channel()
-        queue = await channel.declare_queue(self.consume_queue)
+        queue = await channel.declare_queue(self.consume_queue_name, durable=True)
         await queue.consume(self.process_incoming_message, no_ack=False)
         return connection
 
     async def process_incoming_message(self, message):
         """Processing incoming message from RabbitMQ"""
-        message.ack()
+        await message.ack()
         body = message.body
         print('Received message')
         if body:
@@ -44,10 +49,8 @@ class PikaClient:
             exchange='',
             routing_key=self.publish_queue_name,
             properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
+                reply_to=self.publish_callback_queue,
                 correlation_id=str(uuid.uuid4())
             ),
             body=json.dumps(message)
         )
-
-
